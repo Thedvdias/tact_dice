fe1b779cedb66ba10b5567b680a99a6f93d95563
Daniel Dias <dv.dias@campus.fct.unl.pt>
2025-06-06 23:20:36 +0100

    fix path

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
-Hooks.once('diceSoNiceInit', (dice3d) => {
-    dice3d.addTexture("Tactician", {
-        name: "tactician",
-        composite: "source-over",
-        source: "modules/tact-dice/images/tactician_swirl.png",
-        bump:"modules/tact-dice/images/tactician_swirl.png"
-    });
-
+Hooks.once("diceSoNiceInit", (dice3d) => {
+  dice3d.addTexture("Tactician", {
+    name: "tactician",
+    composite: "source-over",
+    source: "modules/tactician-dice/images/tactician_swirl.png",
+    bump: "modules/tactician-dice/images/tactician_swirl.png",
+  });
 });
