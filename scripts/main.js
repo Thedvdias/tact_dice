bc9555f48fc4871dd8181cd48468743a548c852a
Daniel Dias <dv.dias@campus.fct.unl.pt>
2025-06-06 22:50:37 +0100

    First

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+Hooks.once('diceSoNiceInit', (dice3d) => {
+    dice3d.addTexture("Tactician", {
+        name: "tactician",
+        composite: "source-over",
+        source: "modules/tact-dice/images/tactician_swirl.png",
+        bump:"modules/tact-dice/images/tactician_swirl.png"
+    });
+
+});
